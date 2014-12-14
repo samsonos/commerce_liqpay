a9d364d21984ed32a409464e7bf09b7acd6a26dd
Kotenko <nick.w2r@gmail.com>
2014-12-14 17:59:55 +0200

    add createForm function

--- src/Liqpay.php
+++ src/Liqpay.php
@@ -16,5 +16,63 @@ use samson\core\Config;
  */
 class Liqpay extends CompressableService
 {
-    public $id = 'commerce_liqpay';
+    public $id = 'liqpay';
+
+    public $publicKey;
+    public $privateKey;
+    public $resultUrl;
+
+    private $gate;
+
+
+
+    public function init()
+    {
+        parent::init();
+
+        $this->gate = new \LiqPay($this->publicKey, $this->privateKey);
+        if (!isset($this->resultUrl)) {
+            $this->resultUrl = url_build('/');
+        }
+    }
+
+    public function createForm($Payment)
+    {
+        return $this->gate->cnb_form(array(
+            'version'        => '3',
+            'amount'         => $Payment->Amount,
+            'currency'       => $Payment->Currency,
+            'description'    => t("Оплата за заказ - ").$Payment->id,
+            'order_id'       => $Payment->id,
+            'result_url'     => $this->resultUrl,
+            'server_url'     => url_build('liqpay','status')
+        ));
+    }
+
+    public function __status()
+    {
+        if (isset($_POST['data'])  )
+        {
+            $data = $_POST['data'];
+            $hash = $this->gate->str_to_sign($this->privateKey.$data.$this->privateKey);
+            $result = json_decode(base64_decode($data));
+
+            if ($hash == $_POST['signature'])
+            {
+                $paymentId = (string)$result->order_id;
+
+                if( (string)$result->status == 'success'){
+
+                }
+                if((string) $result->status == 'wait_secure'){
+
+                }
+
+                if((string) $result->status == 'failure'){
+
+                }
+            }
+        }
+        url()->redirect();
+    }
 }
